fa1f4009771092fd621ff16362cd638eaa22500c
Kesington Abdulwakeel <155372577+kesington18@users.noreply.github.com>
2026-09-11 20:38:07 +0100

    Solve Append Characters to String to Make Subsequence

---
+++ 2486-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        n, m = len(s), len(t)
+        l,r = 0, 0
+
+        while l < n and r < m:
+            if s[l] == t[r]:
+                r += 1
+            l += 1
+
+        return m - r
